5784a07c9fb3f3ccd72dc3d393b256ae4ad01231
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-05-24 17:58:13 -0700

    Updating for product pictures

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import Grid from '@material-ui/core'
+import {Grid} from '@material-ui/core'
+
+import Product from './Product/Product'
 
 const products = [
-    {id: 1, name: 'Dragons', description: 'Dragons', price: '$100'},
-    {id: 2, name: 'Characters', description: 'Characters', price: '$30'},
+    {id: 1, name: 'Dragons', description: 'Dragons', price: '$100', image: 'https://i.etsystatic.com/22428673/r/il/210c47/3008107858/il_794xN.3008107858_o6r0.jpg'},
+    {id: 2, name: 'Characters', description: 'Characters', price: '$30', image: 'https://i.etsystatic.com/22428673/r/il/894ca9/2943734845/il_794xN.2943734845_t551.jpg'},
 ]
 
 const Products = () => {
+    return(
 <main>
 
 <Grid container justify="center" spacing={4}> 
 
 {products.map((product) => (
-          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+          <Grid item key={product.id} item xs={12} sm={6} md={4} lg={3}>
             <Product product={product}/> 
           </Grid>
 ))}
@@ -22,7 +25,7 @@ const Products = () => {
 
 
 </main>
-
+    )
 }
 
 export default Products; 
