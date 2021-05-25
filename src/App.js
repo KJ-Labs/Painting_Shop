8f2cc10b0d3094ef2e4ad7da85d3f9325d84f3e8
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-05-24 19:21:00 -0700

    updating for cart button to work

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import {commerce} from './lib/commerce';
-import {Products, Navbar} from './components';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar } from './components';
 
 
 const App = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState({});
     const fetchProducts = async () => {
-        const {data} = await commerce.products.list();
+        const { data } = await commerce.products.list();
 
         setProducts(data);
     }
 
-useEffect(() => {
-fetchProducts();
-}, []);
+
+    const fetchCart = async () => {
+        await commerce.cart.retrieve();
+        setCart(cart)
+
+    }
 
 
+    const handleAddToCart = async (proudctId, quantity) => {
+        const item = await commerce.cart.add(proudctId, quantity);
+        setCart(item.cart);
+
+
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+
+    console.log(cart)
     return (
         <div>
 
-<Navbar/>
-<Products products={products}/>
+            <Navbar />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
 
-export default App; 
+export default App;
