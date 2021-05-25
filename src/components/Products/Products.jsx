2cf0c58b08c59d62b320aa518bb21e0eeb98a808
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-05-24 19:05:35 -0700

    Updating for commerce login and actual products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,9 @@ import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
 import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'Dragons', description: 'Dragons', price: '$100', image: 'https://i.etsystatic.com/22428673/r/il/210c47/3008107858/il_794xN.3008107858_o6r0.jpg' },
-    { id: 2, name: 'Characters', description: 'Characters', price: '$30', image: 'https://i.etsystatic.com/22428673/r/il/894ca9/2943734845/il_794xN.2943734845_t551.jpg' },
-]
 
-const Products = () => {
+
+const Products = ({products}) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
