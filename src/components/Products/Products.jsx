b673bf4c2160305e2e3e68414b3f0c49128a9df8
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2021-05-24 18:28:23 -0700

    Updating for navbar to work with mobile friendly design

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-
-import {Grid} from '@material-ui/core'
-
+import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
+import useStyles from './styles'
 
 const products = [
-    {id: 1, name: 'Dragons', description: 'Dragons', price: '$100', image: 'https://i.etsystatic.com/22428673/r/il/210c47/3008107858/il_794xN.3008107858_o6r0.jpg'},
-    {id: 2, name: 'Characters', description: 'Characters', price: '$30', image: 'https://i.etsystatic.com/22428673/r/il/894ca9/2943734845/il_794xN.2943734845_t551.jpg'},
+    { id: 1, name: 'Dragons', description: 'Dragons', price: '$100', image: 'https://i.etsystatic.com/22428673/r/il/210c47/3008107858/il_794xN.3008107858_o6r0.jpg' },
+    { id: 2, name: 'Characters', description: 'Characters', price: '$30', image: 'https://i.etsystatic.com/22428673/r/il/894ca9/2943734845/il_794xN.2943734845_t551.jpg' },
 ]
 
 const Products = () => {
-    return(
-<main>
+    const classes = useStyles();
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
 
-<Grid container justify="center" spacing={4}> 
 
-{products.map((product) => (
-          <Grid item key={product.id} item xs={12} sm={6} md={4} lg={3}>
-            <Product product={product}/> 
-          </Grid>
-))}
-
-</Grid>
-
-
-</main>
+        </main>
     )
 }
 
-export default Products; 
+export default Products;
